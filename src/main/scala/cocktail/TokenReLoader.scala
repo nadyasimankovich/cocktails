0de7806f6280a8df41d4664d278504b907927132
n.simankovich <nadyasimankovich@gmail.com>
2020-01-03 01:44:51 +0300

    fix

--- src/main/scala/cocktail/TokenReLoader.scala
+++ src/main/scala/cocktail/TokenReLoader.scala
@@ -1,14 +1,15 @@
 package cocktail
 
+import zio.clock.Clock
 import zio.duration._
 import zio.{Schedule, ZIO}
 
 case class TokenState(token: String, ttl: Int)
 
-class TokenReLoader(cocktailDbClient: CocktailDbClient) extends {
+class TokenReLoader(cocktailDbClient: CocktailDbClient) {
   private val policy = Schedule.fixed(60.seconds)
 
-  def reload() = {
+  def reload(): ZIO[Clock, Throwable, Int] = {
     ZIO.effect(cocktailDbClient.reloadToken)
       .repeat(policy)
   }
