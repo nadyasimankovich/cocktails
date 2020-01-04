fa7a423726d6175d14f406555dc2cdfdef0c305a
n.simankovich <nadyasimankovich@gmail.com>
2020-01-04 22:57:37 +0300

    fix

--- src/main/scala/cocktail/TokenReLoader.scala
+++ src/main/scala/cocktail/TokenReLoader.scala
@@ -9,11 +9,12 @@ case class TokenState(token: String, ttl: Int)
 class TokenReLoader(cocktailDbClient: CocktailDbClient) {
   private val policy = Schedule.fixed(60.seconds)
 
-  def reload(): ZIO[Clock, Throwable, Int] = {
+  def reload(): ZIO[Clock, Throwable, Option[Int]] = {
     ZIO.fromFuture { implicit ec =>
       import core.FutureUtils._
       cocktailDbClient.reloadToken.asScala
     }.repeat(policy)
+      .timeout(50.millis)
       .mapError { _ =>
         new Throwable("Something bad happened when token has been updated")
       }
