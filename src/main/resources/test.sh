44bce5a1d613f955deec473cc7f41e4366505267
n.simankovich <nadyasimankovich@gmail.com>
2020-02-20 23:03:11 +0300

    test script

---
+++ src/main/resources/test.sh
@@ -0,0 +1,8 @@
+#bin/bash
+curl -H "Content-Type: application/json" -X POST -d '{"name":"my cocktail","ingredients":"ingredient1,ingredients2,ingredients3","recipe":"shake it"}' http://localhost:8080/add
+curl -X PUT -F image=@photo_2019-12-21_18-30-47.jpg http://localhost:8080/images/my%20cocktail/add
+curl -X GET http://localhost:8080/get?name=my%20cocktail
+curl -X GET http://localhost:8080/images/my%20cocktail -o test.jpg
+
+curl -X GET http://localhost:8080/search?query=margarita
+curl -X GET http://localhost:8080/search?ingredients=rom
